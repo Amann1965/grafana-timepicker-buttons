62e2c102f0e7f7bfae01dd64ad772dcba084e657
Amann1965 <71771450+Amann1965@users.noreply.github.com>
2020-11-18 15:23:47 +0100

    Update Panel.tsx

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -15,7 +15,7 @@ export const Panel: React.FC<Props> = ({ options, data, width, height }) => {
   data.series.forEach(series => {
     let button: TimepickerData = {
       text: series.fields.find(field => field.name === 'button_text')?.values.get(0),
-      time_from: series.fields.find(field => field.name === 'time_from')?.values.get(0),
+      Dt: series.fields.find(field => field.name === 'Dt')?.values.get(0),
       time_to: series.fields.find(field => field.name === 'time_to')?.values.get(0),
       errors: [],
     };
@@ -28,17 +28,17 @@ export const Panel: React.FC<Props> = ({ options, data, width, height }) => {
       }
     }
 
-    // Sanitize time_from
-    if (typeof button.time_from === 'undefined' || button.time_from === null) {
-      button.errors.push('time_from column is required');
+    // Sanitize Dt
+    if (typeof button.Dt === 'undefined' || button.Dt === null) {
+      button.errors.push('Dt column is required');
     } else {
       // Check it's a valid UNIX timestamp
-      button.time_from = Number(button.time_from);
-      if (isNaN(button.time_from)) {
-        button.errors.push('time_from is not a valid UNIX timestamp');
+      button.Dt = Number(button.Dt);
+      if (isNaN(button.Dt)) {
+        button.errors.push('Dt is not a valid UNIX timestamp');
       } else if (typeof button.text === 'undefined' || button.text === null) {
         // If there's no button_text column then just default to a formatted timestamp appropriate for the user's locale
-        button.text = new Date(button.time_from).toLocaleString();
+        button.text = new Date(button.Dt).toLocaleString();
       } else {
         // Make sure text is a string
         button.text = String(button.text);
@@ -70,7 +70,7 @@ export const Panel: React.FC<Props> = ({ options, data, width, height }) => {
       {buttons.map(button => (
         <TimepickerButton
           text={button.text}
-          time_from={button.time_from}
+          Dt={button.Dt}
           time_to={button.time_to}
           primary={button.primary}
           errors={button.errors}
